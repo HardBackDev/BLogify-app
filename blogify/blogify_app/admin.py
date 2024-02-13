aa5b3d75d8827f488dd5a1e9724b04a0a984955b
HardDev <abdullaconechno@gmail.com>
2024-02-14 00:33:14 +0600

    Initial commit

---
+++ blogify/blogify_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from blogify.blogify_app.models import Post
+
+admin.site.register(Post)
---
+++ blogify/blogify_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlogifyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blogify.blogify_app'
